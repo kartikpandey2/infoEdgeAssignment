293258d1969fed4ed2627b87c20cf165fff1950b
kartikpandey2 <kartikpandey2@gmail.com>
2020-09-04 20:51:13 +0530

    Updates

---
+++ precache-manifest.2ff6344130be3de4d5895f3e9c561d5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb51b89d28f5caaa6a044b44adcf6532",
+    "url": "/infoEdgeAssignment/index.html"
+  },
+  {
+    "revision": "feca09099f95e5e67c14",
+    "url": "/infoEdgeAssignment/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "8b3c2d54d3105091800c",
+    "url": "/infoEdgeAssignment/static/css/main.d5227663.chunk.css"
+  },
+  {
+    "revision": "feca09099f95e5e67c14",
+    "url": "/infoEdgeAssignment/static/js/2.b763baf3.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/infoEdgeAssignment/static/js/2.b763baf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3c2d54d3105091800c",
+    "url": "/infoEdgeAssignment/static/js/main.672afe2c.chunk.js"
+  },
+  {
+    "revision": "ac9c4b064fd6f6f02f7a",
+    "url": "/infoEdgeAssignment/static/js/runtime-main.59474704.js"
+  }
+]);
